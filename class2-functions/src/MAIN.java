55812f37ff1235fd2c58a628ea604be4e063831d
NehallBaig <nehallbaig@gmail.com>
2024-04-26 23:52:08 +0500

    Explain function

---
+++ class2-functions/src/MAIN.java
@@ -0,0 +1,12 @@
+
+public class MAIN {
+    public static void main(String arg[]) {
+        Test A = new Test();
+        //A.x = 12;
+        //A.y = 19;
+        System.out.println(A.exp(3));
+        double w=Math.sqrt(34);
+        System.out.println(w);
+        //double p = Math.pow(12, 12);
+    }
+}
